d8941c45e5a24b57ddb578a0ecdc61eddbae9954
marc antonyc <marcosignacio.vr@gmail.com>
2025-10-06 11:22:47 -0300

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.7"
     `maven-publish`
-
 }
 
 group = "com.github.marcosignacio-vr"
@@ -23,20 +19,10 @@ publishing {
             version = "0.0.2"
         }
     }
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/marcosignacio-vr/common-lib")
-            credentials {
-                username = "marcosignacio-vr"
-                password = ""
-            }
-        }
-    }
+
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
